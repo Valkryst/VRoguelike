ca4ca799cb4d7a3f7395ee324da2ede69a91bbaa
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-06 12:03:13 -0300

    Updates tests to match API. Also changes class name.

--- test/com/valkryst/VRoguelike/components/HealthStatTest.java
+++ test/com/valkryst/VRoguelike/components/HealthStatTest.java
@@ -1,38 +1,39 @@
 package com.valkryst.VRoguelike.components;
 
+import com.valkryst.VRoguelike.components.stats.HealthStat;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
 
-public class HealthComponentTest {
-    private HealthComponent component;
+public class HealthStatTest {
+    private HealthStat component;
 
     @Before
     public void initializeComponent() {
-        component = new HealthComponent(123);
+        component = new HealthStat(123);
     }
     @Test
     public void testConstructor_withValidMaxHealth() {
         for (int i = 1 ; i < 10 ; i++) {
-            final HealthComponent component = new HealthComponent(i);
-            Assert.assertEquals(i, component.getCurHealth());
+            final HealthStat component = new HealthStat(i);
+            Assert.assertEquals(i, component.getCurrValue().intValue());
             Assert.assertEquals(i, component.getMaxHealth());
         }
     }
 
     @Test
     public void testConstructor_withNegativeMaxHealth() {
-        final HealthComponent component = new HealthComponent(-1);
-        Assert.assertEquals(1, component.getCurHealth());
+        final HealthStat component = new HealthStat(-1);
+        Assert.assertEquals(1, component.getCurrValue().intValue());
         Assert.assertEquals(1, component.getMaxHealth());
     }
 
     @Test
     public void testToString() {
         final String actual = component.toString();
-        final String expected = "HealthComponent:\n\tCurrent Health:\t123\n\tMaximum Health:\t123";
+        final String expected = "HealthStat:\n\tName:\tHealth\n\tCurrent Health:\t123\n\tMaximum Health:\t123";
         Assert.assertEquals(expected, actual);
     }
 
@@ -48,38 +49,38 @@ public class HealthComponentTest {
 
     @Test
     public void testEquals_withEqualObject() {
-        final HealthComponent componentB = new HealthComponent(123);
+        final HealthStat componentB = new HealthStat(123);
         Assert.assertEquals(component, componentB);
     }
 
     @Test
     public void testEquals_withNonEqualObjects() {
-        final HealthComponent componentB = new HealthComponent(456);
+        final HealthStat componentB = new HealthStat(456);
         Assert.assertNotEquals(component, componentB);
     }
 
     @Test
     public void testToJson() {
         final String actual = component.toJson();
-        final String expected = "{\"curHealth\":123,\"maxHealth\":123}";
+        final String expected = "{\"name\":Health,\"curHealth\":123,\"maxHealth\":123}";
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testSetCurrentHealth_withValidValue() {
         component.setCurrentHealth(65);
-        Assert.assertEquals(65, component.getCurHealth());
+        Assert.assertEquals(65, component.getCurrValue().intValue());
     }
 
     @Test
     public void testSetCurrentHealth_withNegativeValue() {
         component.setCurrentHealth(-456);
-        Assert.assertEquals(0, component.getCurHealth());
+        Assert.assertEquals(0, component.getCurrValue().intValue());
     }
 
     @Test
     public void testSetCurrentHealth_withOverMaxValue() {
         component.setCurrentHealth(456);
-        Assert.assertEquals(123, component.getCurHealth());
+        Assert.assertEquals(123, component.getCurrValue().intValue());
     }
 }
