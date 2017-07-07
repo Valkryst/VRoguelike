8a4acd064de981a79f2bebf9d26d0419e68bd114
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-07 15:01:30 -0300

    Initial commit.

---
+++ test/com/valkryst/VRoguelike/components/stats/IntellectStatTest.java
@@ -0,0 +1,10 @@
+package com.valkryst.VRoguelike.components.stats;
+
+import org.junit.Test;
+
+public class IntellectStatTest {
+    @Test
+    public void testConstructor() {
+        new IntellectStat(5);
+    }
+}
