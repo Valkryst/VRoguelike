4ffc33e250dd8e67559af7d3c831df25ddb0a6b9
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-07 12:19:11 -0300

    Initial commit.

---
+++ src/com/valkryst/VRoguelike/components/InputComponent.java
@@ -0,0 +1,74 @@
+package com.valkryst.VRoguelike.components;
+
+import com.valkryst.VECS.Component;
+import com.valkryst.VRadio.Radio;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputComponent extends Component implements KeyListener {
+    /** The radio. */
+    @Getter @Setter private Radio<KeyEvent> radio;
+
+    /** Constructs a new InputComponent. */
+    public InputComponent() {
+        radio = new Radio<>();
+    }
+
+    @Override
+    public String toString() {
+        String res = getClass().getSimpleName() + ":";
+        res += "\n\tRadio:\t" + radio;
+        return res;
+    }
+
+    @Override
+    public boolean equals(final Object otherObj) {
+        if (otherObj instanceof InputComponent == false) {
+            return false;
+        }
+
+        if (otherObj == this) {
+            return true;
+        }
+
+        final InputComponent otherComp = (InputComponent) otherObj;
+        return radio.equals(otherComp.getRadio());
+    }
+
+    @Override
+    public String toJson() {
+        throw new UnsupportedOperationException("Function not implemented.");
+    }
+
+    /**
+     * Constructs a new InputComponent.
+     *
+     * @param radio
+     *        The radio.
+     */
+    public InputComponent(final Radio<KeyEvent> radio) {
+        if (radio == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.radio = radio;
+    }
+
+    @Override
+    public void keyTyped(final KeyEvent e) {
+        radio.transmit("KEY_TYPED", e);
+    }
+
+    @Override
+    public void keyPressed(final KeyEvent e) {
+        radio.transmit("KEY_PRESSED", e);
+    }
+
+    @Override
+    public void keyReleased(final KeyEvent e) {
+        radio.transmit("KEY_RELEASED", e);
+    }
+}
