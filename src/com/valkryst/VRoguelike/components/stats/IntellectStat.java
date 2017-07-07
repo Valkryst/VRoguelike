8a4865c3da496559fad328c6aa2fcd2f399257ea
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-07 15:01:00 -0300

    Initial commit.

---
+++ src/com/valkryst/VRoguelike/components/stats/IntellectStat.java
@@ -0,0 +1,13 @@
+package com.valkryst.VRoguelike.components.stats;
+
+public class IntellectStat extends StatComponent {
+    /**
+     * Constructs a new IntellectStat.
+     *
+     * @param currValue
+     *        The current value.
+     */
+    public IntellectStat(final int currValue) {
+        super("Intellect", 100, currValue, 0);
+    }
+}
