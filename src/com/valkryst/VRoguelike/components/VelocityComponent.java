3d34f298289618471a8b1e28d018f569649c3049
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-07 12:58:17 -0300

    Adds transmit to receive function.

--- src/com/valkryst/VRoguelike/components/VelocityComponent.java
+++ src/com/valkryst/VRoguelike/components/VelocityComponent.java
@@ -108,6 +108,8 @@ public class VelocityComponent extends Component implements Receiver<KeyEvent> {
                 break;
             }
         }
+
+        radio.transmit("VELOCITY_CHANGED", this);
     }
 
     /**
