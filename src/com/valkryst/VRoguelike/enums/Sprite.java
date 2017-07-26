9e0d72b31f91d210a9f4c2d0f17614d89310b279
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-26 11:42:42 -0300

    Removes the need for separate DARK_ values by adding in darkBack/ForegroundColor vars.

--- src/com/valkryst/VRoguelike/enums/Sprite.java
+++ src/com/valkryst/VRoguelike/enums/Sprite.java
@@ -1,14 +1,12 @@
 package com.valkryst.VRoguelike.enums;
 
+import com.valkryst.VTerminal.misc.ColorFunctions;
 import lombok.Getter;
 
 import java.awt.Color;
 
 public enum Sprite {
-    DARK_DIRT('▒', new Color(0x2C1600), new Color(0x1E0C00)),
     DIRT('▒', new Color(0x452F09), new Color(0x372507)),
-
-    DARK_GRASS('▒', new Color(0x242C00), new Color(0x171E00)),
     GRASS('▒', new Color(0x3D4509), new Color(0x303707)),
 
     DARK_WALL('#', new Color(0, 0, 100), new Color(0, 0, 100)),
@@ -22,6 +20,10 @@ public enum Sprite {
     @Getter private final Color backgroundColor;
     /** The foreground color. */
     @Getter private final Color foregroundColor;
+    /** The dark background color. */
+    @Getter private final Color darkBackgroundColor;
+    /** The dark foreground color. */
+    @Getter private final Color darkForegroundColor;
 
     /**
      * Constructs a new Sprite.
@@ -39,5 +41,7 @@ public enum Sprite {
         this.character = character;
         this.backgroundColor = backgroundColor;
         this.foregroundColor = foregroundColor;
+        darkBackgroundColor = ColorFunctions.shade(backgroundColor, 0.1);
+        darkForegroundColor = ColorFunctions.shade(foregroundColor, 0.1);
     }
 }
