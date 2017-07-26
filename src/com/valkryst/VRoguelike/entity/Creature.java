d3c3ea00a91a6125589331b5a00a3abb56f2691d
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-26 10:19:02 -0300

    Initial commit.

---
+++ src/com/valkryst/VRoguelike/entity/Creature.java
@@ -0,0 +1,66 @@
+package com.valkryst.VRoguelike.entity;
+
+import com.valkryst.VRoguelike.enums.Sprite;
+import com.valkryst.VRoguelike.stat.ResourceStatistic;
+import com.valkryst.VRoguelike.stat.Statistic;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Creature extends Entity {
+    /** The statistics. */
+    private final Map<String, Statistic> statistics = new HashMap<>();
+
+    /**
+     * Constructs a new Creature.
+     *
+     * @param x
+     *        The x-axis position.
+     *
+     * @param y
+     *        The y-axis position.
+     *
+     * @param sprite
+     *        The sprite.
+     */
+    public Creature(final int x, final int y, final Sprite sprite) {
+        super(x, y, sprite);
+
+        addStatistic(new ResourceStatistic("Health", 0, 100));
+        addStatistic(new Statistic("Strength", 1));
+        addStatistic(new Statistic("Defense", 1));
+    }
+
+    /**
+     * Adds a statistic to the Entity.
+     *
+     * @param statistic
+     *        The statistic.
+     *
+     * @throws NullPointerException
+     *        If the statistic is null.
+     */
+    public void addStatistic(final Statistic statistic) {
+        Objects.requireNonNull(statistic);
+        statistics.put(statistic.getName(), statistic);
+    }
+
+    /**
+     * Retrieves a statistic, if it exists.
+     *
+     * @param name
+     *        The statistic's name.
+     *
+     * @return
+     *        The statistic.
+     *
+     * @throws NullPointerException
+     *        If the name is null.
+     */
+    public Optional<Statistic> getStatistic(final String name) {
+        Objects.requireNonNull(name);
+        return Optional.of(statistics.get(name));
+    }
+}
