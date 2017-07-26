7d3d4051d8e3b205647c5adf6f838854d3df7ddb
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-26 15:03:54 -0300

    Initial commit.

---
+++ src/com/valkryst/VRoguelike/action/EquipAction.java
@@ -0,0 +1,47 @@
+package com.valkryst.VRoguelike.action;
+
+import com.valkryst.VRoguelike.entity.Creature;
+import com.valkryst.VRoguelike.entity.Entity;
+import com.valkryst.VRoguelike.item.EquipmentSlot;
+import com.valkryst.VRoguelike.item.EquippableItem;
+import com.valkryst.VRoguelike.world.Map;
+import lombok.Getter;
+
+import java.util.Objects;
+
+public class EquipAction implements Action {
+    /** The slot to equip the item to. */
+    @Getter private final EquipmentSlot slot;
+    /** The item being equipped. */
+    @Getter private final EquippableItem item;
+
+    /**
+     * Constructs a new EquipAction.
+     *
+     * @param slot
+     *        The slot to equip the item to.
+     *
+     * @param item
+     *        The item being equipped.
+     *
+     * @throws NullPointerException
+     *        If the slot or item are null.
+     */
+    public EquipAction(final EquipmentSlot slot, final EquippableItem item) {
+        Objects.requireNonNull(slot);
+        Objects.requireNonNull(item);
+
+        this.slot = slot;
+        this.item = item;
+    }
+
+    @Override
+    public void perform(final Map map, final Entity entity) {
+        if (entity instanceof Creature == false) {
+            return;
+        }
+
+        final Creature creature = (Creature) entity;
+        creature.getEquipment().setItemInSlot(slot, item);
+    }
+}
