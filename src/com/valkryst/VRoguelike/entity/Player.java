c70cce15b8c9ada1097b3ef6b8f762748c36ece1
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-26 14:19:01 -0300

    Removes unnecessary code in toString.

--- src/com/valkryst/VRoguelike/entity/Player.java
+++ src/com/valkryst/VRoguelike/entity/Player.java
@@ -25,9 +25,7 @@ public class Player extends Creature implements KeyListener {
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Player").append(super.toString().substring(8));
-        return sb.toString();
+        return "Player" + super.toString().substring(8);
     }
 
     @Override
