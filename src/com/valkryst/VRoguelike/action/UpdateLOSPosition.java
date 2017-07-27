c6e110e23d7cb0ed95da4d4f490ab89f3830bc90
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-27 12:29:38 -0300

    Adds JavaDoc.
    Reorganizes the perform function.

--- src/com/valkryst/VRoguelike/action/UpdateLOSPosition.java
+++ src/com/valkryst/VRoguelike/action/UpdateLOSPosition.java
@@ -1,5 +1,6 @@
 package com.valkryst.VRoguelike.action;
 
+import com.valkryst.VRoguelike.LineOfSight;
 import com.valkryst.VRoguelike.entity.Creature;
 import com.valkryst.VRoguelike.entity.Entity;
 import com.valkryst.VRoguelike.entity.Player;
@@ -7,13 +8,29 @@ import com.valkryst.VRoguelike.world.Map;
 import com.valkryst.VRoguelike.world.Tile;
 import com.valkryst.VTerminal.component.Screen;
 
+import java.awt.Point;
 import java.util.Objects;
 
 public class UpdateLOSPosition implements Action {
+    /** The screen. */
     private final Screen screen;
+    /** The change in x-axis position. */
     private final int dx;
+    /** The change in y-axis position. */
     private final int dy;
 
+    /**
+     * Constructs a new UpdateLOSPosition.
+     *
+     * @param screen
+     *        The screen.
+     *
+     * @param dx
+     *        The change in x-axis position.
+     *
+     * @param dy
+     *        The change in y-axis position.
+     */
     public UpdateLOSPosition(final Screen screen, final int dx, final int dy) {
         Objects.requireNonNull(screen);
 
@@ -29,8 +46,16 @@ public class UpdateLOSPosition implements Action {
         }
 
         final Tile[][] tiles = map.getTiles();
+        final Creature creature = (Creature) entity;
+
+        if (tiles[creature.getX()][creature.getY()].isSolid()) {
+            return;
+        }
+
+        creature.getLineOfSight().move(dx, dy);
 
         if (entity instanceof Player) {
+            // Set all tiles to non-visible:
             // todo Update to only reset tiles at the player's last position.
             for (int x = 0 ; x < tiles.length ; x++) {
                 for (int y = 0 ; y < tiles[x].length ; y++) {
@@ -40,9 +65,22 @@ public class UpdateLOSPosition implements Action {
                     }
                 }
             }
-        }
 
-        final Creature creature = (Creature) entity;
-        creature.getLineOfSight().move(dx, dy);
+            // Set visible tiles:
+            final LineOfSight los = creature.getLineOfSight();
+
+            los.getLinePoints().forEach(line -> {
+                for (final Point point : line) {
+                    final Tile tile = tiles[point.x][point.y];
+                    tile.setVisible(true);
+                    tile.setVisited(true);
+                    tile.placeOnScreen(screen, point.x, point.y);
+
+                    if (tile.isSolid()) {
+                        break;
+                    }
+                }
+            });
+        }
     }
 }
