bec5d2e223b77cfde73ba24cc7e0f927aaa420c7
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-07-31 14:35:07 -0300

    Initial commit.

---
+++ src/com/valkryst/VRoguelike/item/equipment/Weapon.java
@@ -0,0 +1,42 @@
+package com.valkryst.VRoguelike.item.equipment;
+
+import com.valkryst.VRoguelike.stat.Statistic;
+
+import java.util.List;
+
+public class Weapon extends EquippableItem {
+    /**
+     * Constructs a new Armor.
+     *
+     * @param name
+     *        The name.
+     *
+     * @param description
+     *        The description.
+     *
+     * @param slot
+     *        The slot.
+     */
+    public Weapon(final String name, final String description, final EquipmentSlot slot) {
+        super(name, description, slot);
+    }
+
+    /**
+     * Constructs a new Armor.
+     *
+     * @param name
+     *        The name.
+     *
+     * @param description
+     *        The description.
+     *
+     * @param statistics
+     *        The statistics.
+     *
+     * @param slot
+     *        The slot.
+     */
+    public Weapon(final String name, final String description, final List<Statistic> statistics, final EquipmentSlot slot) {
+        super(name, description, statistics, slot);
+    }
+}
