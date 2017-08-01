2a4bb36a23957ac48499c2b6a053fcc0fd033a2f
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-01 10:58:33 -0300

    Adds another constructor.

--- src/com/valkryst/VRoguelike/world/Map.java
+++ src/com/valkryst/VRoguelike/world/Map.java
@@ -1,6 +1,7 @@
 package com.valkryst.VRoguelike.world;
 
 import com.valkryst.VRoguelike.entity.Entity;
+import com.valkryst.VRoguelike.enums.Sprite;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -21,6 +22,28 @@ public class Map {
      */
     public Map(final Tile[][] tiles) {
         this.tiles = tiles;
+    }
+
+    /**
+     * Constructs a new Map.
+     *
+     * @param width
+     *        The width of the map to create.
+     *
+     * @param height
+     *        The height of the map to create.
+     *
+     * @param defaultSprite
+     *        The default sprite of the tiles to fill the map with.
+     */
+    public Map(final int width, final int height, final Sprite defaultSprite) {
+        tiles = new Tile[width][height];
+
+        for (int x = 0 ; x < width ; x++) {
+            for (int y = 0 ; y < height ; y++) {
+                tiles[x][y] = new Tile(defaultSprite);
+            }
+        }
     }
 
     /** Updates the map. */
