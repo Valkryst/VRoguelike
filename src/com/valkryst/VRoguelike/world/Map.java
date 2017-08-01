2d367de8cf6f3b85adc5739f392bf40f64560761
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-01 11:06:17 -0300

    Adds screen param.

--- src/com/valkryst/VRoguelike/world/Map.java
+++ src/com/valkryst/VRoguelike/world/Map.java
@@ -1,12 +1,17 @@
 package com.valkryst.VRoguelike.world;
 
 import com.valkryst.VRoguelike.entity.Entity;
+import com.valkryst.VTerminal.component.Screen;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Map {
+    /** The screen on which the map is drawn. */
+    @Getter private Screen screen;
+
     /** The tiles. */
     @Getter private Tile[][] tiles;
 
@@ -16,23 +21,41 @@ public class Map {
     /**
      * Constructs a new Map.
      *
+     * @param screen
+     *        The screen on which the map is drawn.
+     *
      * @param tiles
      *        The tiles.
+     *
+     * @throws NullPointerException
+     *         If the screen or tiles are null.
      */
-    public Map(final Tile[][] tiles) {
+    public Map(final Screen screen, final Tile[][] tiles) {
+        Objects.requireNonNull(screen);
+        Objects.requireNonNull(tiles);
+
+        this.screen = screen;
         this.tiles = tiles;
     }
 
     /**
      * Constructs a new Map.
      *
+     * @param screen
+     *        The screen on which the map is drawn.
+     *
      * @param width
      *        The width of the map to create.
      *
      * @param height
      *        The height of the map to create.
+     *
+     * @throws NullPointerException
+     *         If the screen is null.
      */
-    public Map(final int width, final int height) {
+    public Map(final Screen screen, final int width, final int height) {
+        this.screen = screen;
+
         tiles = new Tile[width][height];
 
         for (int x = 0 ; x < width ; x++) {
@@ -54,7 +77,18 @@ public class Map {
      *        The entity.
      */
     public void addEntity(final Entity entity) {
+
         entities.add(entity);
+    }
+
+    /**
+     * Removes an entity from the map.
+     *
+     * @param entity
+     *        The entity.
+     */
+    public void removeEntity(final Entity entity) {
+        entities.remove(entity);
     }
 
     /** @return The width, in tiles, of the map. */
