5c46ac1a6551d2f06ea67a2b9db7aa12a7306b90
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-01 12:02:31 -0300

    Removes KeyListener functionality.

--- src/com/valkryst/VRoguelike/entity/Player.java
+++ src/com/valkryst/VRoguelike/entity/Player.java
@@ -1,13 +1,8 @@
 package com.valkryst.VRoguelike.entity;
 
-import com.valkryst.VRoguelike.action.MoveAction;
-import com.valkryst.VRoguelike.action.UpdateLOSPosition;
 import com.valkryst.VRoguelike.entity.builder.AbstractPlayerBuilder;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-public class Player extends Creature implements KeyListener {
+public class Player extends Creature {
     /**
      * Constructs a new player.
      *
@@ -39,43 +34,5 @@ public class Player extends Creature implements KeyListener {
         }
 
         return super.equals(otherObj);
-    }
-
-    @Override
-    public void keyTyped(final KeyEvent e) {}
-
-    @Override
-    public void keyPressed(final KeyEvent e) {}
-
-    @Override
-    public void keyReleased(final KeyEvent e) {
-        int dx = 0;
-        int dy = 0;
-
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:
-            case KeyEvent.VK_W: {
-                dy = -1;
-                break;
-            }
-            case KeyEvent.VK_DOWN:
-            case KeyEvent.VK_S: {
-                dy = 1;
-                break;
-            }
-            case KeyEvent.VK_LEFT:
-            case KeyEvent.VK_A: {
-                dx = -1;
-                break;
-            }
-            case KeyEvent.VK_RIGHT:
-            case KeyEvent.VK_D: {
-                dx = 1;
-                break;
-            }
-        }
-
-        super.addAction(new MoveAction(dx, dy));
-        super.addAction(new UpdateLOSPosition(dx, dy));
     }
 }
