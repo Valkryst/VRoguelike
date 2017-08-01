f1a38bd23d6f13d23a4bee55bf6546d13dfec161
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-01 13:16:35 -0300

    Replaces statistic map with stat objects.

--- src/com/valkryst/VRoguelike/entity/builder/AbstractCreatureBuilder.java
+++ src/com/valkryst/VRoguelike/entity/builder/AbstractCreatureBuilder.java
@@ -5,19 +5,26 @@ import com.valkryst.VRoguelike.item.equipment.EquipmentInventory;
 import com.valkryst.VRoguelike.item.equipment.EquipmentSlot;
 import com.valkryst.VRoguelike.item.equipment.EquippableItem;
 import com.valkryst.VRoguelike.stat.LimitedStatistic;
-import com.valkryst.VRoguelike.stat.Statistic;
 import lombok.Getter;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public abstract class AbstractCreatureBuilder<B extends AbstractEntityBuilder<B>> extends AbstractEntityBuilder<B> {
     /** The race. */
     @Getter private Race race;
 
-    /** The statistics. */
-    @Getter private Map<String, Statistic> statistics;
+    /** The level. */
+    @Getter private LimitedStatistic stat_level;
+    /** The experience towards the next level. */
+    @Getter private LimitedStatistic stat_xp;
+    /** The amount of gold carried. */
+    @Getter private LimitedStatistic stat_gold;
+    /** The health. */
+    @Getter private LimitedStatistic stat_health;
+    /** The strength. */
+    @Getter private LimitedStatistic stat_strength;
+    /** The defense. */
+    @Getter private LimitedStatistic stat_defense;
 
     /** The equipment inventory. */
     @Getter private EquipmentInventory equipment;
@@ -31,40 +38,35 @@ public abstract class AbstractCreatureBuilder<B extends AbstractEntityBuilder<B>
         super.setName("Creature");
         super.setDescription("This is an unnamed Creature.");
         race = null;
-        statistics = new HashMap<>();
         equipment = new EquipmentInventory();
         lineOfSightRadius = 4;
 
         // Set Stats:
-        final LimitedStatistic xp = new LimitedStatistic("XP", 0, 83);
-        final LimitedStatistic level = new LimitedStatistic("Level", 1, 1, 200);
-
-        addStatistic(xp);
-        addStatistic(level);
-        addStatistic(new LimitedStatistic("Gold", 0, 0, Integer.MAX_VALUE));
-
-        addStatistic(new LimitedStatistic("Health", 0, 100));
-        addStatistic(new Statistic("Strength", 1));
-        addStatistic(new Statistic("Defense", 1));
+        stat_level = new LimitedStatistic("Level", 1, 1, 200);
+        stat_xp = new LimitedStatistic("XP", 0, 83);
+        stat_gold = new LimitedStatistic("Gold", 0, 0, Integer.MAX_VALUE);
+        stat_health = new LimitedStatistic("Health", 0, 100);
+        stat_strength = new LimitedStatistic("Strength", 0, 100);
+        stat_defense = new LimitedStatistic("Defense", 0, 100);
 
         // Level When XP Full:
-        xp.setOnChange(() -> {
-            if (xp.getValue() == xp.getMaximum()) {
-                level.setValue(level.getValue() + 1);
+        stat_xp.setOnChange(() -> {
+            if (stat_xp.getValue() == stat_xp.getMaximum()) {
+                stat_level.setValue(stat_level.getValue() + 1);
             }
         });
 
         // Set New XP Goal on Levelup:
-        level.setOnChange(() -> {
+        stat_level.setOnChange(() -> {
             double newXp = 100;
 
-            for (int i = 1 ; i < level.getValue() ; i++) {
+            for (int i = 1 ; i < stat_level.getValue() ; i++) {
                 newXp += newXp * 0.089;
             }
 
-            xp.setMinimum(0);
-            xp.setValue(0);
-            xp.setMaximum((int) newXp);
+            stat_xp.setMinimum(0);
+            stat_xp.setValue(0);
+            stat_xp.setMaximum((int) newXp);
         });
     }
 
@@ -89,16 +91,86 @@ public abstract class AbstractCreatureBuilder<B extends AbstractEntityBuilder<B>
     }
 
     /**
-     * Adds a statistic.
+     * Sets the level stat.
      *
-     * @param statistic
-     *        The statistic.
+     * @param stat_level
+     *        The level stat.
      *
      * @return
      *        This.
      */
-    public B addStatistic(final Statistic statistic) {
-        statistics.put(statistic.getName(), statistic);
+    public B setStat_level(final LimitedStatistic stat_level) {
+        this.stat_level = stat_level;
+        return (B) this;
+    }
+
+    /**
+     * Sets the xp stat.
+     *
+     * @param stat_xp
+     *        The xp stat.
+     *
+     * @return
+     *        This.
+     */
+    public B setStat_xp(final LimitedStatistic stat_xp) {
+        this.stat_xp = stat_xp;
+        return (B) this;
+    }
+
+    /**
+     * Sets the gold stat.
+     *
+     * @param stat_gold
+     *        The gold stat.
+     *
+     * @return
+     *        This.
+     */
+    public B setStat_gold(final LimitedStatistic stat_gold) {
+        this.stat_gold = stat_gold;
+        return (B) this;
+    }
+
+    /**
+     * Sets the health stat.
+     *
+     * @param stat_health
+     *        The health stat.
+     *
+     * @return
+     *        This.
+     */
+    public B setStat_health(final LimitedStatistic stat_health) {
+        this.stat_health = stat_health;
+        return (B) this;
+    }
+
+    /**
+     * Sets the strength stat.
+     *
+     * @param stat_strength
+     *        The strength stat.
+     *
+     * @return
+     *        This.
+     */
+    public B setStat_strength(final LimitedStatistic stat_strength) {
+        this.stat_strength = stat_strength;
+        return (B) this;
+    }
+
+    /**
+     * Sets the defense stat.
+     *
+     * @param stat_defense
+     *        The defense stat.
+     *
+     * @return
+     *        This.
+     */
+    public B setStat_defense(final LimitedStatistic stat_defense) {
+        this.stat_defense = stat_defense;
         return (B) this;
     }
 
