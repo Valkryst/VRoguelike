fdaaec3c49626f51686daab4d52637576426e76a
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-02 12:37:39 -0300

    Initial commit.

---
+++ src/com/valkryst/VRoguelike/ai/PassiveCombatAI.java
@@ -0,0 +1,9 @@
+package com.valkryst.VRoguelike.ai;
+
+import com.valkryst.VRoguelike.entity.Creature;
+import com.valkryst.VRoguelike.world.Map;
+
+public class PassiveCombatAI extends CombatAI {
+    @Override
+    public void decide(final Map map, final Creature self, final Creature target) {}
+}
