b9b32a24833f546eba543adfc5680748dc8b4e31
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-08 09:44:00 -0300

    Sets sprite in constructor.

--- src/com/valkryst/VRoguelike/entity/builder/PlayerBuilder.java
+++ src/com/valkryst/VRoguelike/entity/builder/PlayerBuilder.java
@@ -1,9 +1,11 @@
 package com.valkryst.VRoguelike.entity.builder;
 
 import com.valkryst.VRoguelike.entity.Player;
+import com.valkryst.VRoguelike.enums.Sprite;
 
 public class PlayerBuilder extends CreatureBuilder {
     public Player build() {
+        super.setSprite(Sprite.PLAYER);
         super.checkState();
         return new Player(this);
     }
