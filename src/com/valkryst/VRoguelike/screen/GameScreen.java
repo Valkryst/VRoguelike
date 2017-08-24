0c5fc178203c7cdd34342239183dfdfe0403931f
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-08-24 11:22:32 -0300

    Adds mockup UI.

--- src/com/valkryst/VRoguelike/screen/GameScreen.java
+++ src/com/valkryst/VRoguelike/screen/GameScreen.java
@@ -1,19 +1,60 @@
 package com.valkryst.VRoguelike.screen;
 
+import com.valkryst.VRadio.Radio;
 import com.valkryst.VRoguelike.world.Map;
 import com.valkryst.VTerminal.Panel;
+import com.valkryst.VTerminal.builder.component.TextAreaBuilder;
 import com.valkryst.VTerminal.component.Screen;
+import com.valkryst.VTerminal.component.TextArea;
+import com.valkryst.VTerminal.printer.RectanglePrinter;
 import lombok.Getter;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GameScreen extends Screen implements KeyListener {
-    @Getter private final Map map = new Map(this, 90, 30);
+    @Getter private final Map map = new Map(this, 65, 25);
+
+    @Getter private TextArea messageBox;
 
     public GameScreen(final Panel panel) {
         super(0, 0, panel.getWidthInCharacters(), panel.getHeightInCharacters());
         panel.addKeyListener(this);
+
+        drawUISections();
+        createMessageBox(panel.getRadio());
+    }
+
+    private void drawUISections() {
+        final RectanglePrinter printer = new RectanglePrinter();
+        printer.setWidth(23);
+        printer.setHeight(10);
+
+        // Player UI Section:
+        printer.setTitle("Player");
+        printer.print(this, 66, 0);
+
+
+        // Target UI Section:
+        printer.setTitle("Target");
+        printer.print(this, 66, 11);
+    }
+
+    private void createMessageBox(final Radio<String> radio) {
+        final TextAreaBuilder builder = new TextAreaBuilder();
+
+        builder.setRadio(radio);
+
+        builder.setColumnIndex(0);
+        builder.setRowIndex(25);
+
+        builder.setWidth(65);
+        builder.setHeight(5);
+
+        builder.setEditable(false);
+
+        messageBox = builder.build();
+        this.addComponent(messageBox);
     }
 
     @Override
