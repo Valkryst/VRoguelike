b463c62aa561e05dc0ec8bfd069e522d067325be
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-09-26 10:44:49 -0300

    Updated to make better use of Lombok, also replaces StringBuilder with an inline string.

--- src/com/valkryst/VRoguelike/item/Item.java
+++ src/com/valkryst/VRoguelike/item/Item.java
@@ -2,28 +2,26 @@ package com.valkryst.VRoguelike.item;
 
 import com.valkryst.VRoguelike.item.builder.ItemBuilder;
 import com.valkryst.VRoguelike.stat.Statistic;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-import java.util.Objects;
-
+@Data
 public class Item {
     /** The name. */
-    @Getter @Setter private String name = "Item";
+    private String name = "Item";
 
     /** The description.. */
-    @Getter @Setter private String description = "This is an unnamed Item.";
+    private String description = "This is an unnamed Item.";
 
     /** The health. */
-    @Getter private final Statistic stat_health;
+    private final Statistic stat_health;
     /** The strength. */
-    @Getter private final Statistic stat_strength;
+    private final Statistic stat_strength;
     /** The defense. */
-    @Getter private final Statistic stat_defense;
+    private final Statistic stat_defense;
     /** The accuracy (Percent chance to land an attack). */
-    @Getter private final Statistic stat_accuracy;
+    private final Statistic stat_accuracy;
     /** The dodge (Percent chance to dodge an attack). */
-    @Getter private final Statistic stat_dodge;
+    private final Statistic stat_dodge;
 
     /**
      * Constructs a new Item.
@@ -44,44 +42,14 @@ public class Item {
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append(this.getClass().getSimpleName()).append(":");
-        sb.append("\n\tName:\t").append(name);
-        sb.append("\n\tDescription:\t").append(description);
-        sb.append("\n\tStatistics:");
-        sb.append(stat_health.toString().replace("\n\t", "\n\t\t"));
-        sb.append(stat_strength.toString().replace("\n\t", "\n\t\t"));
-        sb.append(stat_defense.toString().replace("\n\t", "\n\t\t"));
-        sb.append(stat_accuracy.toString().replace("\n\t", "\n\t\t"));
-        sb.append(stat_dodge.toString().replace("\n\t", "\n\t\t"));
-
-        return sb.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, description, stat_health, stat_strength, stat_defense, stat_accuracy, stat_dodge);
-    }
-
-    @Override
-    public boolean equals(final Object otherObj) {
-        if (otherObj instanceof Item == false) {
-            return false;
-        }
-
-        if (otherObj == this) {
-            return true;
-        }
-
-        final Item otherItm = (Item) otherObj;
-
-        boolean isEqual = Objects.equals(name, otherItm.getName());
-        isEqual &= Objects.equals(description, otherItm.getDescription());
-        isEqual &= Objects.equals(stat_health, otherItm.getStat_health());
-        isEqual &= Objects.equals(stat_strength, otherItm.getStat_strength());
-        isEqual &= Objects.equals(stat_defense, otherItm.getStat_defense());
-        isEqual &= Objects.equals(stat_accuracy, otherItm.getStat_accuracy());
-        isEqual &= Objects.equals(stat_dodge, otherItm.getStat_dodge());
-        return isEqual;
+        return this.getClass().getSimpleName() + ":" +
+                "\n\tName:\t" + name +
+                "\n\tDescription:\t" + description +
+                "\n\tStatistics:" +
+                stat_health.toString().replace("\n\t", "\n\t\t") +
+                stat_strength.toString().replace("\n\t", "\n\t\t") +
+                stat_defense.toString().replace("\n\t", "\n\t\t") +
+                stat_accuracy.toString().replace("\n\t", "\n\t\t") +
+                stat_dodge.toString().replace("\n\t", "\n\t\t");
     }
 }
