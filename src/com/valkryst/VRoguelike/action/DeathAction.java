8eaa943b36e1f46b78aea5622e58d49d22ed082a
Valkryst <valkryst@valkryst.com>
2017-10-02 11:50:59 -0300

    Updates perform function to drop items on death.

--- src/com/valkryst/VRoguelike/action/DeathAction.java
+++ src/com/valkryst/VRoguelike/action/DeathAction.java
@@ -1,6 +1,9 @@
 package com.valkryst.VRoguelike.action;
 
+import com.valkryst.VRoguelike.entity.Creature;
 import com.valkryst.VRoguelike.entity.Entity;
+import com.valkryst.VRoguelike.entity.builder.ItemDropBuilder;
+import com.valkryst.VRoguelike.loot.LootTable;
 import com.valkryst.VRoguelike.world.Map;
 import lombok.Data;
 import lombok.NonNull;
@@ -11,5 +14,20 @@ public class DeathAction implements Action {
     public void perform(final @NonNull Map map, final @NonNull Entity entity) {
         map.removeEntities(entity);
         map.getMessageBox().appendText(entity.getName() + " has died.");
+
+
+        if (entity instanceof Creature) {
+            final Creature creature = (Creature) entity;
+            final LootTable lootTable = creature.getLootTable();
+
+            if (lootTable != null) {
+                lootTable.loot().forEach(lootEntry -> {
+                    final ItemDropBuilder builder = new ItemDropBuilder();
+                    builder.setItem(lootEntry);
+
+                    map.addEntities(builder.build());
+                });
+            }
+        }
     }
 }
