c54a39908feced32ecd01b17c8acf5708787fe5f
Valkryst <valkryst@valkryst.com>
2017-10-02 08:29:53 -0300

    Updates to work with the new version of VTerminal.

--- src/com/valkryst/VRoguelike/action/MoveAction.java
+++ src/com/valkryst/VRoguelike/action/MoveAction.java
@@ -7,16 +7,15 @@ import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
 
+import java.awt.Point;
 import java.util.List;
 import java.util.Objects;
 
 @EqualsAndHashCode
 @ToString
 public class MoveAction implements Action {
-    /** The current position on the x-axis. */
-    private final int x;
-    /** The current position on the y-axis. */
-    private final int y;
+    /** The current position on the x/y-axis. */
+    private final Point position;
     /** The change in x-axis position. */
     private final int dx;
     /** The change in y-axis position. */
@@ -25,11 +24,8 @@ public class MoveAction implements Action {
     /**
      * Constructs a new MoveAction.
      *
-     * @param x
-     *        The current position on the x-axis.
-     *
-     * @param y
-     *        The current position on the y-axis.
+     * @param position
+     *        The current position on the x/y-axis.
      *
      * @param dx
      *        The change in x-axis position.
@@ -37,9 +33,8 @@ public class MoveAction implements Action {
      * @param dy
      *        The change in y-axis position.
      */
-    public MoveAction(final int x, final int y, final int dx, final int dy) {
-        this.x = x;
-        this.y = y;
+    public MoveAction(final Point position, final int dx, final int dy) {
+        this.position = position;
         this.dx = dx;
         this.dy = dy;
     }
@@ -49,8 +44,10 @@ public class MoveAction implements Action {
         Objects.requireNonNull(map);
         Objects.requireNonNull(entity);
 
+        final Point newPosition = new Point(position.x + dx, position.y + dy);
+
         // Attack any enemies at new location:
-        final List<Entity> entities = map.getEntityAt(x + dx, y + dy);
+        final List<Entity> entities = map.getEntityAt(newPosition);
 
         if (entities.size() >= 1) {
             for (final Entity target : entities) {
@@ -62,10 +59,9 @@ public class MoveAction implements Action {
             return;
         }
 
-        if (map.isPositionFree(x + dx, y + dy)) {
-            entity.getLayer().setColumnIndex(x + dx);
-            entity.getLayer().setRowIndex(y + dy);
-            new UpdateLOSAction(x, y, dx, dy).perform(map, entity);
+        if (map.isPositionFree(newPosition)) {
+            entity.setPosition(newPosition);
+            new UpdateLOSAction(position, dx, dy).perform(map, entity);
         }
     }
 }
