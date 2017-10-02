1a60817d9361c586b18e3919375172f9aa8101c7
Valkryst <valkryst@valkryst.com>
2017-10-02 12:39:52 -0300

    Removes secondary constructor and switches from width/height to Dimension var.

--- src/com/valkryst/VRoguelike/world/Room.java
+++ src/com/valkryst/VRoguelike/world/Room.java
@@ -3,29 +3,24 @@ package com.valkryst.VRoguelike.world;
 import com.valkryst.VRoguelike.enums.Sprite;
 import com.valkryst.VRoguelike.screen.GameScreen;
 
+import java.awt.Dimension;
 import java.awt.Point;
 
 public class Room {
     private final Point position;
-    private int width;
-    private int height;
+    final Dimension dimensions;
 
-    public Room(final int columnIndex, final int rowIndex, final int width, final int height) {
-        this(new Point(columnIndex, rowIndex), width, height);
-    }
-
-    public Room(final Point position, final int width, final int height) {
+    public Room(final Point position, final Dimension dimensions) {
         this.position = position;
-        this.width = width;
-        this.height = height;
+        this.dimensions = dimensions;
     }
 
     public void carve(final GameScreen screen) {
         final Map map = screen.getMap();
         final Tile[][] tiles = map.getTiles();
 
-        for (int x = position.x ; x < position.x + width ; x++) {
-            for (int y = position.y ; y < position.y + height ; y++) {
+        for (int x = position.x ; x < position.x + dimensions.width ; x++) {
+            for (int y = position.y ; y < position.y + dimensions.height ; y++) {
                 tiles[x][y].setSprite(Sprite.DIRT);
                 tiles[x][y].setSolid(false);
             }
