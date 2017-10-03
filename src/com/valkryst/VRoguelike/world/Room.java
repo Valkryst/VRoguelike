b85a1eee306df1c52edcd61add4500abe4ab31fa
Tyler MacEachern <tyler.maceachern@bluespurs.com>
2017-10-03 16:06:20 -0300

    Adds JavaDoc, sets dimensions to private, and changes param on carve function.

--- src/com/valkryst/VRoguelike/world/Room.java
+++ src/com/valkryst/VRoguelike/world/Room.java
@@ -1,22 +1,37 @@
 package com.valkryst.VRoguelike.world;
 
 import com.valkryst.VRoguelike.enums.Sprite;
-import com.valkryst.VRoguelike.screen.GameScreen;
 
 import java.awt.Dimension;
 import java.awt.Point;
 
 public class Room {
+    /** The x/y-axis coordinate of the top-left character of the room. */
     private final Point position;
-    final Dimension dimensions;
+    /** The width/height of the room. */
+    private final Dimension dimensions;
 
+    /**
+     * Constructs a new Room.
+     *
+     * @param position
+     *          The x/y-axis coordinates of the top-left character of the
+     *          room.
+     *
+     * @param dimensions
+     */
     public Room(final Point position, final Dimension dimensions) {
         this.position = position;
         this.dimensions = dimensions;
     }
 
-    public void carve(final GameScreen screen) {
-        final Map map = screen.getMap();
+    /**
+     * Carves-out the room on the map.
+     *
+     * @param map
+     *          The map.
+     */
+    public void carve(final Map map) {
         final Tile[][] tiles = map.getTiles();
 
         for (int x = position.x ; x < position.x + dimensions.width ; x++) {
