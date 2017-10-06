81954698588c4138473546a017b8c02e7ba33d54
Valkryst <valkryst@valkryst.com>
2017-10-06 12:33:03 -0300

    Adds check for cached path.

--- src/com/valkryst/VRoguelike/ai/movement/BFSMovementAI.java
+++ src/com/valkryst/VRoguelike/ai/movement/BFSMovementAI.java
@@ -9,6 +9,75 @@ import java.util.ArrayDeque;
 public class BFSMovementAI extends MovementAI {
     @Override
     public ArrayDeque<Point> findPath(final @NonNull Map map, final @NonNull Point start, final @NonNull Point end) {
-        return new ArrayDeque<>();
+        // Check if cached path exists:
+        final ArrayDeque<Point> path = super.findPath(map, start, end);
+
+        if (path.size() > 0) {
+            return path;
+        }
+
+
+        final boolean[][] visitedNodes = new boolean[map.getHeight()][map.getWidth()];
+        final ArrayDeque<MovementNode> nodes = new ArrayDeque<>();
+
+        MovementNode endNode = null;
+        boolean endFound = false;
+
+        // Add start node
+        nodes.add(new MovementNode(map, null, start));
+
+        while (nodes.size() > 0 && endFound == false) {
+            final MovementNode currentNode = nodes.removeFirst();
+            final Point currentPosition = currentNode.getCurrentPosition();
+
+            // Skip previously visited nodes
+            if (visitedNodes[currentPosition.y][currentPosition.x]) {
+                continue;
+            } else {
+                visitedNodes[currentPosition.y][currentPosition.x] = true;
+            }
+
+            // Add new neighbours to the queue
+            final Point[] neighbours = super.getNeighbours(map, currentPosition);
+
+            for (final Point neighbour : neighbours) {
+                if (neighbour == null || visitedNodes[neighbour.y][neighbour.x]) {
+                    continue;
+                }
+
+                nodes.add(new MovementNode(map, currentNode, neighbour));
+
+                // Check if the neighbour is the destination
+                if (end.equals(neighbour)) {
+                    endFound = true;
+                    endNode = nodes.getLast();
+                }
+            }
+        }
+
+        return constructPath(endNode);
+    }
+
+    /**
+     * Constructs a path of points from the end node to
+     * the start node.
+     *
+     * @param endNode
+     *          The end node.
+     *
+     * @return
+     *          A FILO queue where the end position is
+     *          at the beginning of the queue and the
+     *          start position is at the end of the queue.
+     */
+    private ArrayDeque<Point> constructPath(@NonNull MovementNode endNode) {
+        final ArrayDeque<Point> path = new ArrayDeque<>();
+
+        while (endNode != null) {
+            path.add(endNode.getCurrentPosition());
+            endNode = endNode.getPreviousMovement();
+        }
+
+        return path;
     }
 }
