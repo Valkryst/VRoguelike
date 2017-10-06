7e7a82f25c456fe5bc23ab7354fc1bd6c3a4bf93
Valkryst <valkryst@valkryst.com>
2017-10-06 11:12:18 -0300

    Switches previousPosition for previousMovement.

--- src/com/valkryst/VRoguelike/ai/movement/MovementNode.java
+++ src/com/valkryst/VRoguelike/ai/movement/MovementNode.java
@@ -6,25 +6,25 @@ import lombok.NonNull;
 import java.awt.Point;
 
 public class MovementNode {
-    /** The previous position of the movement. */
-    @Getter private final Point previousPosition;
-    /** The current position of the movement. */
+    /** The previous movement. */
+    @Getter private final MovementNode previousMovement;
+    /** The current position. */
     @Getter private final Point currentPosition;
 
     /**
      * Constructs a new MovementNode.
      *
-     * @param previousPosition
-     *          The previous position of the movement.
+     * @param previousMovement
+     *          The previous movement.
      *
      * @param currentPosition
-     *          The current position of the movement.
+     *          The current position.
      *
      * @throws NullPointerException
      *          If the currentPosition is null.
      */
-    public MovementNode(final Point previousPosition, final @NonNull Point currentPosition) {
-        this.previousPosition = previousPosition;
+    public MovementNode(final MovementNode previousMovement, final @NonNull Point currentPosition) {
+        this.previousMovement = previousMovement;
         this.currentPosition = currentPosition;
     }
 }
