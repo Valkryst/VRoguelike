bb295d273be47d115e3a53bc8494d43bf27cc461
Valkryst <valkryst@valkryst.com>
2017-10-06 12:33:44 -0300

    Removes overridden function.

--- src/com/valkryst/VRoguelike/ai/movement/AStarMovementAI.java
+++ src/com/valkryst/VRoguelike/ai/movement/AStarMovementAI.java
@@ -1,14 +1,4 @@
 package com.valkryst.VRoguelike.ai.movement;
 
-import com.valkryst.VRoguelike.world.Map;
-import lombok.NonNull;
-
-import java.awt.Point;
-import java.util.ArrayDeque;
-
 public class AStarMovementAI extends MovementAI {
-    @Override
-    public ArrayDeque<Point> findPath(final @NonNull Map map, final @NonNull Point start, final @NonNull Point end) {
-        return new ArrayDeque<>();
-    }
 }
--- src/com/valkryst/VRoguelike/ai/movement/DFSMovementAI.java
+++ src/com/valkryst/VRoguelike/ai/movement/DFSMovementAI.java
@@ -1,14 +1,4 @@
 package com.valkryst.VRoguelike.ai.movement;
 
-import com.valkryst.VRoguelike.world.Map;
-import lombok.NonNull;
-
-import java.awt.Point;
-import java.util.ArrayDeque;
-
 public class DFSMovementAI extends MovementAI {
-    @Override
-    public ArrayDeque<Point> findPath(final @NonNull Map map, final @NonNull Point start, final @NonNull Point end) {
-        return new ArrayDeque<>();
-    }
 }
