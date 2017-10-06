c9756f682dd66b8623218f910aaeb6876e639c87
Valkryst <valkryst@valkryst.com>
2017-10-06 10:36:07 -0300

    Initial commit of the MovementNode.

---
+++ src/com/valkryst/VRoguelike/ai/movement/MovementNode.java
@@ -0,0 +1,27 @@
+package com.valkryst.VRoguelike.ai.movement;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.awt.Point;
+
+public class MovementNode {
+    /** The previous position of the movement. */
+    @Getter private final Point previousPosition;
+    /** The current position of the movement. */
+    @Getter private final Point currentPosition;
+
+    /**
+     * Constructs a new MovementNode.
+     *
+     * @param previousPosition
+     *          The previous position of the movement.
+     *
+     * @param currentPosition
+     *          The current position of the movement.
+     */
+    public MovementNode(final @NonNull Point previousPosition, final @NonNull Point currentPosition) {
+        this.previousPosition = previousPosition;
+        this.currentPosition = currentPosition;
+    }
+}
