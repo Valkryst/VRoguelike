390cbc353a89f43a15db782de62cf008f0e7389d
Valkryst <valkryst@valkryst.com>
2017-10-10 10:28:28 -0300

    Removes some unnecessary code.

--- src/com/valkryst/VRoguelike/Driver.java
+++ src/com/valkryst/VRoguelike/Driver.java
@@ -6,7 +6,6 @@ import com.valkryst.VRoguelike.entity.Creature;
 import com.valkryst.VRoguelike.entity.Player;
 import com.valkryst.VRoguelike.entity.builder.CreatureBuilder;
 import com.valkryst.VRoguelike.entity.builder.PlayerBuilder;
-import com.valkryst.VRoguelike.enums.Race;
 import com.valkryst.VRoguelike.enums.Sprite;
 import com.valkryst.VRoguelike.item.builder.equipment.WeaponBuilder;
 import com.valkryst.VRoguelike.item.equipment.EquipmentSlot;
@@ -59,7 +58,6 @@ public class Driver {
             final PlayerBuilder playerBuilder = new PlayerBuilder();
             playerBuilder.setX(25);
             playerBuilder.setY(12);
-            playerBuilder.setRace(Race.HUMAN);
             final Player player = playerBuilder.build();
 
             final WeaponBuilder weaponBuilder = new WeaponBuilder();
@@ -79,7 +77,6 @@ public class Driver {
             final CreatureBuilder creatureBuilder = new CreatureBuilder();
             creatureBuilder.setX(26);
             creatureBuilder.setY(13);
-            creatureBuilder.setRace(Race.HUMAN);
             creatureBuilder.setCombatAI(new AggressiveCombatAI());
             creatureBuilder.setSprite(Sprite.ENEMY);
             creatureBuilder.setLootTable(lootTable);
