c4bc6280d1fd171a3eefc53e6b6b80abd8279a1f
Valkryst <valkryst@valkryst.com>
2017-10-10 11:21:10 -0300

    Adds pronouns and UNKNOWN gender.

--- src/com/valkryst/VRoguelike/enums/Gender.java
+++ src/com/valkryst/VRoguelike/enums/Gender.java
@@ -1,6 +1,57 @@
 package com.valkryst.VRoguelike.enums;
 
+import lombok.Getter;
+import lombok.NonNull;
+
 public enum Gender {
-    MALE,
-    FEMALE;
+    MALE("he", "him", "his", "himself"),
+    FEMALE("she", "her", "hers", "herself"),
+    UNKNOWN("it", "it", "its", "itself");
+
+    /** The subject pronoun. (Ex. he) */
+    @Getter private final String pronounSubject;
+    /** The object pronoun. (Ex. him) */
+    @Getter private final String pronounObject;
+    /** The possessive pronoun. (Ex. his) */
+    @Getter private final String pronounPossessive;
+    /** The reflexive pronoun. (Ex. himself) */
+    @Getter private final String pronounReflexive;
+
+    /**
+     * Constructs a new Gender enum.
+     *
+     * @param pronounSubject
+     *          The subject pronoun. (Ex. he)
+     *
+     * @param pronounObject
+     *          The object pronoun. (Ex. him)
+     *
+     * @param pronounPossessive
+     *          The possessive pronoun. (Ex. his)
+     *
+     * @param pronounReflexive
+     *          The reflexive pronoun. (Ex. himself)
+     */
+    Gender(final @NonNull String pronounSubject, final @NonNull String pronounObject, final @NonNull String pronounPossessive, final @NonNull String pronounReflexive) {
+        if (pronounSubject.isEmpty()) {
+            throw new IllegalArgumentException("The subject pronoun cannot be empty.");
+        }
+
+        if (pronounObject.isEmpty()) {
+            throw new IllegalArgumentException("The object pronoun cannot be empty.");
+        }
+
+        if (pronounPossessive.isEmpty()) {
+            throw new IllegalArgumentException("The possessive pronoun cannot be empty.");
+        }
+
+        if (pronounReflexive.isEmpty()) {
+            throw new IllegalArgumentException("The reflexice pronoun cannot be empty.");
+        }
+
+        this.pronounSubject = pronounSubject;
+        this.pronounObject = pronounObject;
+        this.pronounPossessive = pronounPossessive;
+        this.pronounReflexive = pronounReflexive;
+    }
 }
