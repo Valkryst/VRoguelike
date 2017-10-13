d62bdeeff5647bb6fdbe3c4d46620236bc3199fd
Valkryst <valkryst@valkryst.com>
2017-10-13 13:07:49 -0300

    Rewrites class to make use of VJSON.

--- src/com/valkryst/VRoguelike/world/Room.java
+++ src/com/valkryst/VRoguelike/world/Room.java
@@ -1,15 +1,24 @@
 package com.valkryst.VRoguelike.world;
 
+import com.valkryst.VJSON.VJSONParser;
 import com.valkryst.VRoguelike.enums.Sprite;
+import lombok.NonNull;
+import org.json.simple.JSONObject;
 
 import java.awt.Dimension;
 import java.awt.Point;
 
-public class Room {
+public class Room implements VJSONParser {
     /** The x/y-axis coordinate of the top-left character of the room. */
     private final Point position;
     /** The width/height of the room. */
     private final Dimension dimensions;
+
+    /** Constructs a new Room. */
+    public Room() {
+        this.position = new Point(0, 0);
+        this.dimensions = new Dimension(0, 0);
+    }
 
     /**
      * Constructs a new Room.
@@ -39,6 +48,23 @@ public class Room {
                 tiles[x][y].setSprite(Sprite.DIRT);
                 tiles[x][y].setSolid(false);
             }
+        }
+    }
+
+    @Override
+    public void parse(final @NonNull JSONObject jsonObject) {
+        final Integer x = getInteger(jsonObject, "x");
+        final Integer y = getInteger(jsonObject, "y");
+
+        final Integer width = getInteger(jsonObject, "width");
+        final Integer height = getInteger(jsonObject, "height");
+
+        if (x != null && y != null) {
+            this.position.setLocation(x, y);
+        }
+
+        if (width != null && height != null) {
+            this.dimensions.setSize(width, height);
         }
     }
 }
