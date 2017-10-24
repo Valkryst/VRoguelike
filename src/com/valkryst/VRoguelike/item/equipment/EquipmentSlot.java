edb91fde06ff10bbfce7d670df3569c8d4578de4
Valkryst <valkryst@valkryst.com>
2017-10-24 09:35:46 -0300

    Adds getName function.

--- src/com/valkryst/VRoguelike/item/equipment/EquipmentSlot.java
+++ src/com/valkryst/VRoguelike/item/equipment/EquipmentSlot.java
@@ -1,5 +1,7 @@
 package com.valkryst.VRoguelike.item.equipment;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 public enum EquipmentSlot {
     HEAD,
     NECK,
@@ -13,4 +15,20 @@ public enum EquipmentSlot {
     FEET,
     MAIN_HAND,
     OFF_HAND;
+
+    /**
+     * Retrieves the name of the slot, but with the first letter
+     * uppercased and all other characters lowercased. All
+     * underscores are also converted to spaces.
+     *
+     * @return
+     *         The name of the slot.
+     */
+    public String getName() {
+        String name = this.name();
+        name = name.toLowerCase();
+        name = name.replace('_', ' ');
+        name = WordUtils.capitalize(name);
+        return name;
+    }
 }
