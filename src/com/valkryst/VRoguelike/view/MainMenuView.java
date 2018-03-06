b7d8b92d49f03c08050114892b2fd894fbd18d4a
Valkryst <valkryst@valkryst.com>
2018-03-06 13:45:26 -0400

    Updated to work with new VTerminal API.

--- src/com/valkryst/VRoguelike/view/MainMenuView.java
+++ src/com/valkryst/VRoguelike/view/MainMenuView.java
@@ -1,17 +1,20 @@
-package com.valkryst.VRoguelike.screen;
+package com.valkryst.VRoguelike.view;
 
+import com.valkryst.VTerminal.Screen;
 import com.valkryst.VTerminal.builder.ButtonBuilder;
-import com.valkryst.VTerminal.builder.LayerBuilder;
 import com.valkryst.VTerminal.component.Button;
 import com.valkryst.VTerminal.component.Layer;
 import lombok.Getter;
+import lombok.NonNull;
 
-public class MainMenuScreen extends Layer {
+import java.awt.*;
+
+public class MainMenuView extends Layer {
     @Getter private Button button_new;
     @Getter private Button button_exit;
 
-    public MainMenuScreen(final LayerBuilder builder) {
-        super(builder);
+    public MainMenuView(final @NonNull Screen screen) {
+        super(new Dimension(screen.getWidth(), screen.getHeight()));
 
         // Construct menu options:
         final ButtonBuilder buttonBuilder = new ButtonBuilder();
