a4f69f160fef0729a41feb38d20d2954dac7f652
Valkryst <valkryst@valkryst.com>
2018-03-06 13:55:51 -0400

    Updated to work with new VTerminal API.

--- src/com/valkryst/VRoguelike/Driver.java
+++ src/com/valkryst/VRoguelike/Driver.java
@@ -6,48 +6,39 @@ import com.valkryst.VRoguelike.entity.Player;
 import com.valkryst.VRoguelike.entity.builder.PlayerBuilder;
 import com.valkryst.VRoguelike.item.builder.equipment.WeaponBuilder;
 import com.valkryst.VRoguelike.item.equipment.EquipmentSlot;
-import com.valkryst.VRoguelike.screen.GameScreen;
-import com.valkryst.VRoguelike.screen.MainMenuScreen;
-import com.valkryst.VRoguelike.world.Tile;
-import com.valkryst.VTerminal.Panel;
-import com.valkryst.VTerminal.builder.PanelBuilder;
+import com.valkryst.VRoguelike.view.GameView;
+import com.valkryst.VRoguelike.view.MainMenuView;
+import com.valkryst.VRoguelike.world.MapTile;
+import com.valkryst.VTerminal.Screen;
 import com.valkryst.VTerminal.font.Font;
 import com.valkryst.VTerminal.font.FontLoader;
 import org.json.simple.parser.ParseException;
 
-import javax.swing.Timer;
-import java.awt.Point;
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Driver {
-    public static Panel PANEL;
-
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         final Font font = FontLoader.loadFontFromJar("Fonts/DejaVu Sans Mono/18pt/bitmap.png", "Fonts/DejaVu Sans Mono/18pt/data.fnt", 1);
+        final Screen screen = new Screen(120, 40, font);
 
-        final PanelBuilder panelBuilder = new PanelBuilder();
-        panelBuilder.setFont(font);
-        panelBuilder.setWidthInCharacters(120);
-        panelBuilder.setHeightInCharacters(40);
 
-        PANEL = panelBuilder.build();
+        final MainMenuView mainMenuView = new MainMenuView(screen);
+        final GameView gameView = new GameView(screen);
 
-        Thread.sleep(50);
+        screen.addComponent(mainMenuView);
 
-        final MainMenuScreen mainMenuScreen = new MainMenuScreen(PANEL);
-        final GameScreen gameScreen = new GameScreen(PANEL);
-        PANEL.swapScreen(mainMenuScreen);
-
-        mainMenuScreen.getButton_new().setOnClickFunction(() -> {
+        mainMenuView.getButton_new().setOnClickFunction(() -> {
             // Initialize map tiles:
-            PANEL.swapScreen(gameScreen);
+            screen.removeComponent(mainMenuView);
+            screen.addComponent(gameView);
 
-            final Tile[][] tiles = gameScreen.getMap().getTiles();
+            final MapTile[][] tiles = gameView.getMap().getTiles();
 
             for (int x = 0 ; x < tiles.length ; x++) {
                 for (int y = 0 ; y < tiles[x].length ; y++) {
-                    tiles[x][y].placeOnScreen(PANEL.getScreen(), x, y);
+                    tiles[x][y].placeOnScreen(gameView, x, y);
                 }
             }
 
@@ -75,28 +66,28 @@ public class Driver {
             */
 
             try {
-                VJSONLoader.loadFromJSON(gameScreen.getMap(), System.getProperty("user.dir") + "/test_res/test_map.json");
+                VJSONLoader.loadFromJSON(gameView.getMap(), System.getProperty("user.dir") + "/test_res/test_map.json");
             } catch (ParseException | IOException e) {
                 e.printStackTrace();
             }
 
-            gameScreen.getMap().addEntities(player);
+            gameView.getMap().addEntities(player);
 
 
             // Add Player/Creature Screens to main panel
-            gameScreen.setPlayer(player);
+            gameView.setPlayer(player);
 
 
             // Test movement
-            player.getMovementAI().findAndSetPath(gameScreen.getMap(), player.getPosition(), new Point(player.getPosition().x + 3, player.getPosition().y + 5));
+            player.getMovementAI().findAndSetPath(gameView.getMap(), player.getPosition(), new Point(player.getPosition().x + 3, player.getPosition().y + 5));
         });
 
-        mainMenuScreen.getButton_exit().setOnClickFunction(() -> System.exit(0));
+        mainMenuView.getButton_exit().setOnClickFunction(() -> System.exit(0));
 
         // Render loop:
         final Timer timer = new Timer(100, e -> {
-            gameScreen.getMap().update();
-            PANEL.draw();
+            gameView.getMap().update();
+            screen.draw();
         });
 
         timer.start();
