2650e19f339afa7a80e6faa9e0e0512fe6890208
Valkryst <valkryst@valkryst.com>
2018-03-07 11:02:32 -0400

    Adds constructor.

--- src/com/valkryst/VRoguelike/gui/view/View.java
+++ src/com/valkryst/VRoguelike/gui/view/View.java
@@ -3,7 +3,22 @@ package com.valkryst.VRoguelike.gui.view;
 import com.valkryst.VTerminal.component.Layer;
 import lombok.Getter;
 
+import java.awt.*;
+
 public class View {
     /** The layer of the view. */
-    @Getter protected Layer layer;
+    @Getter protected final Layer layer;
+
+    /**
+     * Constructs a new View.
+     *
+     * @param width
+     *          The width of the view's layer.
+     *
+     * @param height
+     *          The height of the view's layer.
+     */
+    public View(final int width, final int height) {
+        layer = new Layer(new Dimension(width, height));
+    }
 }
